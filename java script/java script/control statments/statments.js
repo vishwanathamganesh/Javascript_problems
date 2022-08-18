8f7232ac29e7160641e72da5fb5b0c6ff87bc1d6
vishwanathamganesh <84628376+vishwanathamganesh@users.noreply.github.com>
2022-08-18 20:12:57 +0530

    Add files via upload

---
+++ java script/java script/control statments/statments.js
@@ -0,0 +1,24 @@
+var output=[];
+var count=1;
+
+function fizzbuzz(){
+	//write code here//
+	if (cout%3===0 && count%5===0) {
+		output.push("fizzbuzz");
+	}
+	else if  (count%3===0) {
+		output.push(fizz);
+	} 
+	else if{(count%5===0){
+		output.push(buzz);
+	}
+	else{
+		output.push(count);
+	}
+}
+///to take the right decision between two conditions we use the conditional sataments///
+///there are four types of conditions///
+//if statment //---we use this condition if the condition is true
+//if else statment///-we use tthis statment if the statment is is false we write another code on else that if sttatment is false we use the second condition as solution
+//if else if///
+//switch statment////
---
+++ java script/java script/loops/loops.js
@@ -0,0 +1,39 @@
+///their are four types of loops 
+///while loop// the purpose of while loop is to execute a statment or block of code repetedly as long the condition is true if the condition is false the loop will be exited
+//syntax///
+//while (condition){
+	//block to be exected}
+///
+//for example  give that the condition to print 0 to 5 numbers
+var i=0;
+while(i<=5)
+{
+	document.write("the number is i++");
+	document.write("<br>")
+	i++;
+}
+//if see the out put it will print up to 5numbers and and after that it will not print it will exit//
+
+//do while loop///-this loop will execute the code before hecking if it is true orfalse if tthe condition repeat the loop untill the condition is true
+var i=0;
+while(i<=5)
+{
+	document.write("the number is i++");
+	document.write("<br>")
+	i++;
+}
+while(i<=5);
+
+///for loop///
+///intialize condition increment// this arre the three thing we need to use  to write the for loop//
+
+var i=0;
+for (i=0;i<=10;i++)
+{
+	document.write(numberis "+i")
+	document.write("<br>")
+
+}
+///////////////for in loop///////////
+//syntaxfor loops///////
+
